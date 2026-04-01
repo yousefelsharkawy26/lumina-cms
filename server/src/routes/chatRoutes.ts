843d4bcdfb1b7c064bc6b41d6af49f3698e95c40
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2026-04-01 23:51:42 +0200

    Remove backend, keep frontend only

--- server/src/routes/chatRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { handleChat } from '../controllers/chatController';
-import { protect } from '../middleware/authMiddleware';
-
-const router = express.Router();
-
-router.post('/', protect, handleChat);
-
-export default router;
--- server/src/routes/settingsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getSettings, updateSettings } from "../controllers/settingsController";
-import { protect, admin } from "../middleware/authMiddleware";
-
-const router = express.Router();
-
-router.route("/").get(getSettings).put(protect, admin, updateSettings);
-
-export default router;
